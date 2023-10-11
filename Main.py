7e1d90b33e8028ead63f7b82c58105611a2a568f
Testbot379 <92651609+CodeSent@users.noreply.github.com>
2023-10-11 21:05:04 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,22 @@
-print("hello world")
+import pygame
+from sys import exit
+
+class Engine():
+  def __init__(self, Fps=30):
+    self.Fps= Fps
+    pygame.init()
+    self.Screen = pygame.display.set_mode((500,500))
+    self.Clock = pygame.time.Clock()
+    self.RunLoop()
+
+  def RunLoop(self):
+    while True:
+      for event in pygame.event.get():
+        if event.type== pygame.QUIT:
+          pygame.quit()
+          exit()
+      pygame.display.update()
+      self.Clock.tick(self.Fps)
+
+if __name__ == "__main__":
+  Engine()
